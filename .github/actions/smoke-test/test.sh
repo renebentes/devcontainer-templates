2a11e165c2d5afc857904ab06c35687c569789b3
Rene B. Pinto <renebentes@yahoo.com.br>
2025-01-27 11:49:37 -0300

    style: apply coding standards to shell scripts
    This commit removes possible syntax errors and applies coding standards
    to shell scripts.

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -6,8 +6,9 @@ SRC_DIR="/tmp/${TEMPLATE_ID}"
 echo "Running Smoke Test"
 
 ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/sh -c "if [ -f \"test-project/test.bats\" ]; then cd test-project && ./bats/bin/bats test.bats; else ls -a; fi"
+devcontainer exec --workspace-folder "${SRC_DIR}" --id-label "${ID_LABEL}" /bin/sh -c "if [ -f \"test-project/test.bats\" ]; then cd test-project && ./bats/bin/bats test.bats; else ls -a; fi"
 
 # Clean up
-docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
+echo "Performing Clean up"
+docker rm -f "$(docker container ls -f "label=${ID_LABEL}" -q)"
 rm -rf "${SRC_DIR}"
