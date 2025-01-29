59b501924b8d1dea730c88973da0f40a17b2d87c
Rene B. Pinto <renebentes@yahoo.com.br>
2025-01-29 12:46:22 +0000

    ci: run all test files

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -6,7 +6,8 @@ SRC_DIR="/tmp/${TEMPLATE_ID}"
 echo "Running Smoke Test"
 
 ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer exec --workspace-folder "${SRC_DIR}" --id-label "${ID_LABEL}" /bin/sh -c "if [ -f \"test-project/test.bats\" ]; then cd test-project && ./bats/bin/bats test.bats; else ls -a; fi"
+devcontainer exec --workspace-folder "${SRC_DIR}" --id-label "${ID_LABEL}" \
+    /bin/sh -c "if [ -d \"test-project\" ]; then cd test-project && ./bats/bin/bats .; else ls -a; fi"
 
 # Clean up
 echo "Performing Clean up"
